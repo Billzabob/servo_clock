86de5320ea368aea427d12ce4270162be3f8d520
Nick Hallstrom <nick.hallstrom@divvypay.com>
2021-09-08 22:39:58 -0600

    Add timing to display changes

--- lib/clock/display.ex
+++ lib/clock/display.ex
@@ -1,5 +1,8 @@
 defmodule Clock.Display do
   use GenServer
+
+  require Logger
+
   @min_duty_cycle 2.7
   @max_duty_cycle 12.0
   @min_angle 0
@@ -11,6 +14,16 @@ defmodule Clock.Display do
     GenServer.start_link(__MODULE__, [0, 0, 0, 0, 0, 0, 0], name: __MODULE__)
   end
 
+  @spec get() :: state
+  def get() do
+    GenServer.call(__MODULE__, :get)
+  end
+
+  @spec set(state, integer) :: :ok
+  def set(state, milliseconds \\ 300) do
+    GenServer.cast(__MODULE__, {:set, state, milliseconds})
+  end
+
   @impl true
   @spec init(state) :: {:ok, state}
   def init(state) do
@@ -19,24 +32,31 @@ defmodule Clock.Display do
   end
 
   @impl true
-  def handle_cast({:set, state}, _state) do
-    set_to(state)
-    {:noreply, state}
+  def handle_cast({:set, end_state, milliseconds}, start_state) do
+    start_time = System.monotonic_time(:millisecond)
+    end_time = start_time + milliseconds
+    run(start_state, end_state, start_time, end_time, start_time)
+    {:noreply, end_state}
+  end
+
+  defp run(start_state, end_state, start_time, end_time, current_time)
+       when current_time <= end_time do
+    start_state
+    |> Enum.zip(end_state)
+    |> Enum.map(fn {c, n} -> map(current_time, {start_time, end_time}, {c, n}) end)
+    |> set_to()
+
+    current_time = System.monotonic_time(:millisecond)
+    run(start_state, end_state, start_time, end_time, current_time)
+  end
+
+  defp run(_start_state, end_state, _start_time, _end_time, _current_time) do
+    set_to(end_state)
   end
 
   @impl true
   def handle_call(:get, _from, state) do
     {:reply, state, state}
-  end
-
-  @spec get() :: state
-  def get() do
-    GenServer.call(__MODULE__, :get)
-  end
-
-  @spec set(state) :: :ok
-  def set(state) do
-    GenServer.cast(__MODULE__, {:set, state})
   end
 
   @spec set_to(state) :: :ok
@@ -51,5 +71,12 @@ defmodule Clock.Display do
   end
 
   defp angle_to_duty_cycle(angle),
-    do: angle |> ServoKit.map({@min_angle, @max_angle}, {@min_duty_cycle, @max_duty_cycle})
+    do: angle |> map({@min_angle, @max_angle}, {@min_duty_cycle, @max_duty_cycle})
+
+  defp map(x, {in_min, in_max}, {out_min, out_max})
+       when is_number(x) and
+              is_number(in_min) and is_number(in_max) and
+              is_number(out_min) and is_number(out_max) do
+    (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+  end
 end
